a8838ffe1d906c1a6f2a79ae577e1adbdb4f7d40
Posad <posad.stanislavsky@gmail.com>
2025-02-27 00:14:27 +0300

    refactored main module

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
 import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
+
+from configs.config import Config, load_config
+from keyboards.set_menu import set_main_menu
+
+logger = logging.getLogger()
+logging.basicConfig(level='INFO')
+
+
+async def main():
+    config: Config = load_config()
+    bot = Bot(token=config.tg_bot.token,
+              default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher()
+
+    await set_main_menu(bot)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Бот выключен...')
